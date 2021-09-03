bf64bc25046efe25aceff2545b749fc6576dbb8c
akshat1003 <68696758+akshat1003@users.noreply.github.com>
2021-09-03 15:16:25 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n = 0;cin>>n;
+    int a=0;int b=1;
+    cout<<a<<" "<<b<<" "<<;
+    for(int i=2;i<=n;i++)
+    {
+        c=a+b;
+        cout<<c<<" "<<;
+        a=b;b=c;
+    }
+    cout<<endl;
+}
